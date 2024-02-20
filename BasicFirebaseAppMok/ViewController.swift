7aba2af59f51021921b741a8b85a942ef6c6fb0e
Tyler Mok <tymok06@gmail.com>
2024-02-20 09:44:50 -0600

    fix

--- BasicFirebaseAppMok/ViewController.swift
+++ BasicFirebaseAppMok/ViewController.swift
@@ -6,14 +6,72 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var ref: DatabaseReference!
+    
+    var items = [String]()
+    
+    @IBOutlet weak var itemInputList: UITextField!
+    
+    @IBOutlet weak var itemAmountList: UITextField!
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableViewOutlet.dataSource = self
+        tableViewOutlet.delegate = self
+        ref = Database.database().reference()
+   
+        ref.child("items").observe(.childAdded, with: { (snapshot) in
+                 
+            let n = snapshot.value as! String
+                  
+            if !self.items.contains(n){
+            self.items.append(n)
+                   }
+               })
+        
+        ref.child("items").observeSingleEvent(of: .value, with: { snapshot in
+    print("--inital load has completed and the last user was read--")
+                    print(self.items)
+                    })
+
+
     }
 
-
+    
+    @IBAction func addToListButton(_ sender: Any) {
+        var item = itemInputList.text!
+        var amount = "\(itemAmountList.text!)"
+        ref.child("Items").childByAutoId().setValue(item)
+        ref.child("Item Amount").childByAutoId().setValue(amount)
+        
+        print(item)
+        print(amount)
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    
+    
 }
 
---
+++ Items.swift
@@ -0,0 +1,29 @@
+//
+//  Items.swift
+//  BasicFirebaseAppMok
+//
+//  Created by TYLER MOK on 2/20/24.
+//
+
+import Foundation
+import FirebaseDatabase
+import FirebaseCore
+
+public class Items {
+    
+var item: String = ""
+var quan: Int
+var ref = Database.database().reference()
+var key = ""
+    
+    
+    init(item: String, quan: Int) {
+        self.item = item
+        self.quan = quan
+    }
+    
+    let dict = ["item": item, "amount": quan] as [String: Any]
+    
+    
+    
+}
